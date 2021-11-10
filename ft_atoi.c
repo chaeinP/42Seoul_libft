2417c2e8b23862666a0a9d7972d61446bfc37f5b
chaeinP <pca0046@gmail.com>
2021-11-10 19:58:39 +0900

    211110-part1 complete

---
+++ ft_atoi.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: chaepark <chaepark@student.42seoul.kr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/10 15:01:57 by chaepark          #+#    #+#             */
+/*   Updated: 2021/11/10 19:36:04 by chaepark         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_atoi(const char *str)
+{
+	int	i;
+	int	n;
+	int	result;
+
+	i = 0;
+	n = 1;
+	result = 0;
+	while (str[i] == '\t' || str[i] == '\v' || str[i] == '\n'
+		|| str[i] == ' ' || str[i] == '\f' || str[i] == '\r')
+		i++;
+	if (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			n = -1;
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+		result = result * 10 + str[i++] - 48;
+	return (result * n);
+}
