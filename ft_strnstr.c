fc66a5919b705f7b2e106e742ec2a38b94550392
chaeinP <pca0046@gmail.com>
2021-11-16 01:10:04 +0900

    211116-strnstr 수정

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: chaepark <chaepark@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/10 02:05:19 by chaepark          #+#    #+#             */
-/*   Updated: 2021/11/15 19:37:38 by chaepark         ###   ########.fr       */
+/*   Updated: 2021/11/16 01:00:26 by chaepark         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,8 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 	char	*hay;
 
 	i = 0;
+	if (!haystack && !len)
+		return (0);
 	needle_len = ft_strlen(needle);
 	hay = (char *)haystack;
 	if (!needle_len)
